347ff9a9419c62c5770e9a52d46fea24fc44758c
Andrew <kittridgea@uindy.edu>
2017-02-21 17:18:17 -0500

    added webtest index file

---
+++ WebTest/index.php
@@ -0,0 +1,23 @@
+<html>
+	<title>Webtest</title>
+	<head>
+		Testing the performance
+	</head>
+	<body>
+	<?php 
+	echo '<table border>="1',"\n";
+	for($y = 0; $ < size; y++){
+		echo "<tr>\n"
+		for($x = 0; $x < size; $x++){
+			echo "     <td style=\"background-color:#FF0000\>"
+			echo $x * $y;
+			echo "</td>\n"
+		}
+		echo "<tr>\n"
+	}
+
+	echo "</table\n"
+	?>
+	</body>
+
+</html>
